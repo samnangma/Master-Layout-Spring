a682f7812647f667d8a1f1fb1b33b00cce16b4ec
Samnang MA <masamnang6@gmail.com>
2023-04-20 15:59:26 +0700

    Maste-Layout

---
+++ src/main/java/com/example/springthymeleaffragmenet/FragmentController/FragmentController.java
@@ -0,0 +1,30 @@
+package com.example.springthymeleaffragmenet.FragmentController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Calling fragment using fragment expression
+@Controller
+public class FragmentController {
+    @GetMapping("")
+    String index(){
+        return "index";
+    }
+
+    @GetMapping("/allusers")
+    String allUsers(Model model){
+        List <String> userNames = new ArrayList<>(){{
+            add("James");
+            add("hashaa");
+            add("joe");
+            add("Nana");
+        }};
+        model.addAttribute("users",userNames);
+        return "alluser";
+    }
+
+}
